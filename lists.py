9a72d0a9ddec39e092aff38f0b02e22e966a3bcf
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-05-10 10:26:56 -0400

    Update lists.py

--- lists.py
+++ lists.py
@@ -23,7 +23,7 @@ def is_empty(list_to_check):
         return False
 
 def check_lists(list_to_compare1, list_to_compare2):
-    if len(list_to_compare1) >= 3 and len(list_to_compare1) >= 3: 
+    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3: 
         if list_to_compare1[2] == list_to_compare2[2]:
             return True
         else:
