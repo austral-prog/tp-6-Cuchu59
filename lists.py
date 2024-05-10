8dbbc47172be44f3bd9e109a8f6596a5c3fd2da8
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-05-10 10:07:41 -0400

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,40 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_remove_elements) > 5:
+        del list_to_remove_elements[5]
+    
+    if len(list_to_remove_elements) > 3:
+        del list_to_remove_elements[4]
+    
+    if len(list_to_remove_elements) != 0:
+        del list_to_remove_elements[0]
+    return list_to_remove_elements
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_to_add_elements.append("Yellow")
+    list_to_add_elements.insert(0, "Pink")
+    return list_to_add_elements
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_check) > 0:
+        return False
+    else: 
+        return True
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_compare1) >= 3 and len(list_to_compare1) >= 3: 
+        if list_to_compare1 == list_to_compare2:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    ret_list = []
+    ret_list.append(list_of_lists_to_modify[0][:2] if len(list_of_lists_to_modify) > 2 else (list_of_lists_to_modify[0] if len(list_of_lists_to_modify) > 0 else []))
+    ret_list.append(list_of_lists_to_modify[1][1:4] if len(list_of_lists_to_modify) >= 4 else (list_of_lists_to_modify[1][1:4] if len(list_of_lists_to_modify) >= 3 else []))
+    ret_list.append(list_of_lists_to_modify[2][-2:] if len(list_of_lists_to_modify) >= 2 else (list_of_lists_to_modify[2] if len(list_of_lists_to_modify) > 0 else []))
+    return ret_list
