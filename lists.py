9ab233dfb0465a7d4c3c44da867d591b2510328b
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-05-10 10:19:22 -0400

    Update lists.py

--- lists.py
+++ lists.py
@@ -2,7 +2,7 @@ def remove_elements(list_to_remove_elements):
     if len(list_to_remove_elements) > 5:
         del list_to_remove_elements[5]
     
-    if len(list_to_remove_elements) > 3:
+    if len(list_to_remove_elements) > 4:
         del list_to_remove_elements[4]
     
     if len(list_to_remove_elements) != 0:
@@ -17,10 +17,10 @@ def add_elements(list_to_add_elements):
 
 
 def is_empty(list_to_check):
-    if len(list_to_check) > 0:
-        return False
-    else: 
+    if len(list_to_check) == 0:
         return True
+    else: 
+        return False
 
 def check_lists(list_to_compare1, list_to_compare2):
     if len(list_to_compare1) >= 3 and len(list_to_compare1) >= 3: 
